dbd4e21384295dd6c7d3cfcc209a09fe1134b298
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-20 04:48:34 +0000

    with bug

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,9 +14,131 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+
 /* EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
 */
+TEST(TicTacToeBoardTest, toggleTurn1)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('O', T.toggleTurn());
+}
+
+TEST(TicTacToeBoardTest, toggleTurn2)
+{
+	TicTacToeBoard T;
+	T.toggleTurn();
+	ASSERT_EQ('X', T.toggleTurn());
+}
+
+TEST(TicTacToeBoardTest, placePiece1)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('X', T.placePiece(1,1));
+}
+
+TEST(TicTacToeBoardTest, placePiece2)
+{
+	TicTacToeBoard T;
+	T.placePiece(2,1);
+	ASSERT_EQ('O', T.placePiece(1,1));
+}
+
+TEST(TicTacToeBoardTest, placePiece3)
+{
+	TicTacToeBoard T;
+	T.placePiece(2,1);
+	ASSERT_EQ('X', T.placePiece(2,1));
+}
+
+TEST(TicTacToeBoardTest, placePiece4)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('?', T.placePiece(4,6));
+}
+
+TEST(TicTacToeBoardTest, placePiece5)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('?', T.placePiece(-2,-2));
+}
+
+TEST(TicTacToeBoardTest, getPiece1)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ(' ', T.getPiece(1,1));
+}
+
+TEST(TicTacToeBoardTest, getPiece2)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('?', T.getPiece(4,4));
+}
+
+TEST(TicTacToeBoardTest, getPiece3)
+{
+	TicTacToeBoard T;
+	ASSERT_EQ('?', T.getPiece(-1,-1));
+}
+
+TEST(TicTacToeBoardTest, getPiece4)
+{
+	TicTacToeBoard T;
+	T.placePiece(1,1);
+	ASSERT_EQ('X', T.getPiece(1,1));
+}
+
+TEST(TicTacToeBoardTest, getWinner1)
+{
+	TicTacToeBoard T;
+	T.placePiece(1,1);
+	ASSERT_EQ('?', T.getWinner());
+}
+
+TEST(TicTacToeBoardTest, getWinner2)
+{
+	TicTacToeBoard T;
+	T.placePiece(1,1);
+	T.placePiece(0,0);
+	T.placePiece(0,1);
+	T.placePiece(0,2);
+	T.placePiece(1,0);
+	T.placePiece(1,2);
+	T.placePiece(2,1);
+	T.placePiece(2,0);
+	T.placePiece(2,2);
+	ASSERT_EQ('X', T.getWinner());
+}
+
+TEST(TicTacToeBoardTest, getWinner3)
+{
+	TicTacToeBoard T;
+	T.placePiece(0,1);
+	T.placePiece(0,0);
+	T.placePiece(1,0);
+	T.placePiece(0,2);
+	T.placePiece(1,2);
+	T.placePiece(1,1);
+	T.placePiece(2,1);
+	T.placePiece(2,0);
+	T.placePiece(2,2);
+	ASSERT_EQ('O', T.getWinner());
+}
+
+TEST(TicTacToeBoardTest, getWinner4)
+{
+	TicTacToeBoard T;
+	T.placePiece(0,1);
+	T.placePiece(0,0);
+	T.placePiece(0,2);
+	T.placePiece(1,1);
+	T.placePiece(1,0);
+	T.placePiece(1,2);
+	T.placePiece(2,1);
+	T.placePiece(2,0);
+	T.placePiece(2,2);
+	ASSERT_EQ(' ', T.getWinner());
+}
